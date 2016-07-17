56939ce5a90d2bab6b7d2349fcd9ded1797edbc1
xiaolinyan <zpxocivuby@gmail.com>
2016-07-17 14:19:26 +0800

    tijiao

---
+++ ItafMobileApp/build/generated/source/buildConfig/debug/itaf/mobile/app/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package itaf.mobile.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "itaf.mobile.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1000000174;
+  public static final String VERSION_NAME = "App_20140120.0556.31";
+}
